ed8c20c91cb7b62a5af05b819aadac2e835c4543
Your Name <you@example.com>
2021-01-14 09:44:09 +0000

    first upload

---
+++ Assets/Scripts/SpinnerY.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerY : MonoBehaviour
+{
+    public float speed = 100f;
+    float counter = 0;
+    float frames = 0;
+
+    void Start()
+    {
+        frames = Time.deltaTime;
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+        
+        counter += Time.deltaTime;
+
+        Debug.Log("Time: " + counter);
+        Debug.Log("FPS: " + frames / Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SpinnerZ.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinnerZ : MonoBehaviour
+{
+    public float speed = 100f;
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speed * Time.deltaTime);
+    }
+
+}
